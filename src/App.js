b736669b579424e1333e7d51f647a585f26f2f40
viswanthkj <viswanth1995@gmail.com>
2020-06-13 16:46:25 +0530

    shope page added

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import {
   Route,
 } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-const Hats = () => (
-  <div>
-  <h1>HATS PAGE</h1>
-  </div>
-) 
+// const Hats = () => (
+//   <div>
+//   <h1>HATS PAGE</h1>
+//   </div>
+// ) 
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component={Hats}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
