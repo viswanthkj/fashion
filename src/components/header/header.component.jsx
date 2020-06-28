d5102321a0f6818f05f986d41f9b0ea68f03916d
viswanthkj <viswanth1995@gmail.com>
2020-06-28 18:02:26 +0530

    reducer added for cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { auth } from '../../firebase/firebase.utils'
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
 <div className='header'>
  <Link className='logo-container' to = '/'>
   <Logo className='logo'/>
@@ -26,14 +26,15 @@ const Header = ({ currentUser }) => (
     SIGN IN
     </Link>
 }
-<CartIcon/>
+ <CartIcon/>
 </div>
-<CartDropdown/>
+    {hidden ?  null : <CartDropdown/>}
 </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
