d561ccadcd6976b2da153ffb8bb5bcfb9e343eb5
viswanthkj <viswanth1995@gmail.com>
2020-06-28 17:26:54 +0530

    cart dropdown component added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { auth } from '../../firebase/firebase.utils'
 import './header.styles.scss';
 
@@ -27,6 +28,7 @@ const Header = ({ currentUser }) => (
 }
 <CartIcon/>
 </div>
+<CartDropdown/>
 </div>
 )
 
